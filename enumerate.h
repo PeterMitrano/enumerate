fd695dbf2b4770c42db8e9e4457b90902d04ffd3
Peter Mitrano <mitranopeter@gmail.com>
2020-05-29 10:08:25 -0400

    Update enumerate.h

--- enumerate.h
+++ enumerate.h
@@ -3,29 +3,27 @@
 #include <iterator>
 #include <algorithm>
 
-namespace enmr {
-
-template <class Iterator>
+template <class Itr>
 struct enumerate {
 
-    enumerate(Iterator first, Iterator last) {
-        auto i = 0lu;
-        auto make_pair = [&i] (auto item) auto {
-            return std::make_pair(i++, item);
-        };
-        internal_container.resize(std::distance(first, last));
-        std::transform(first, last, internal_container.begin(), make_pair);
-    }
+  enumerate(Itr v) {
+    auto first = std::begin(v);
+    auto last = std::end(v);
+    auto i = 0lu;
+    auto make_pair = [&i] (auto item) auto {
+      return std::make_pair(i++, item);
+    };
+    internal_container.resize(std::distance(first, last));
+    std::transform(first, last, internal_container.begin(), make_pair);
+  }
 
-    auto begin() const {
-        return internal_container.begin();
-    }
+  auto begin() const {
+    return internal_container.begin();
+  }
 
-    auto end() const {
-        return internal_container.end();
-    }
+  auto end() const {
+    return internal_container.end();
+  }
 
-    std::vector<std::pair<size_t, typename std::iterator_traits<Iterator>::value_type>> internal_container;
+  std::vector<std::pair<size_t, typename Itr::value_type>> internal_container;
 };
-
-} // Namespace enmr
